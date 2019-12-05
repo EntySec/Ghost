e005bab56b7de54afcbd5f3d78b7f03b1118513a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-05 23:44:28 +0300

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -60,16 +60,16 @@ page_1 = '''{2}
 :   :          ` . `.          {0}[{1}4{0}] {2}Access shell on a phone
  `.. :            `. ``;       {0}[{1}5{0}] {2}Install an apk on a phone
     `:;             `:'        {0}[{1}6{0}] {2}Screen record a phone
-       :              `.       {0}[{1}7{0}] {2}ScreenShot a phone
-        `.              `.     {0}[{1}8{0}] {2}Restart Server
+       :              `.       {0}[{1}7{0}] {2}Get phone screenshot
+        `.              `.     {0}[{1}8{0}] {2}Restart ADB Server
           `'`'`'`---..,___`.   {0}[{1}9{0}] {2}Pull folders from phone        
                      
-{0}[{1}10{0}] {2}Turn The Device off       {0}[{1}19{0}]{2} Extract apk from app                         
+{0}[{1}10{0}] {2}Shutdown the phone        {0}[{1}19{0}]{2} Extract apk from app                         
 {0}[{1}11{0}] {2}Uninstall an app          {0}[{1}20{0}]{2} Get Battery Status
-{0}[{1}12{0}] {2}Show log of device        {0}[{1}21{0}]{2} NetStat 
+{0}[{1}12{0}] {2}Show log of device        {0}[{1}21{0}]{2} Network status
 {0}[{1}13{0}] {2}Dump System Info          {0}[{1}22{0}]{2} Turn WiFi on/off
-{0}[{1}14{0}] {2}List all apps on a phone  {0}[{1}23{0}]{2} Remove Password
-{0}[{1}15{0}] {2}Run an app                {0}[{1}24{0}]{2} Use Keycode
+{0}[{1}14{0}] {2}List all phone apps       {0}[{1}23{0}]{2} Remove Password
+{0}[{1}15{0}] {2}Run a phone app           {0}[{1}24{0}]{2} Use Ghost Keycode
 {0}[{1}16{0}]{2} Port Forwarding           {0}[{1}25{0}]{2} Get Current Activity
 {0}[{1}17{0}]{2} Grab wpa_supplicant       {0}[{1}26{0}]{2} Update Ghost Framework
 {0}[{1}18{0}]{2} Show Mac/Inet             {0}[{1}27{0}]{2} Exit Ghost Framework                                                                                                                    
