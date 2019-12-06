b6c5ff3044a5194c7ec7f75f45cea39f0dd42575
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-07 02:00:41 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -19,13 +19,13 @@ RS="\033[0;31m"
 YS="\033[1;33m"
 CE="\033[0;97m"
 C="\033[0m"
-
+GN="\033[0;32m"
 WHS="\033[0;97m"
 
 if [[ $EUID -ne 0 ]]
 then
    sleep 1
-   echo -e ""$CE"["$RS"+"$CE"] This script must be run as root!"$C"" 1>&2
+   echo -e ""$GN"["$RS"+"$GN"]"$CE" This script must be run as root!"$C"" 1>&2
    sleep 1
    exit
 fi
