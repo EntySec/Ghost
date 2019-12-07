9426bec46bba0b9d404f5b0a63b4f81315a14793
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-07 21:38:21 +0300

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -108,15 +108,15 @@ page_1 = '''{2}
          `.              `.    {0}[{1}8{0}] {2}Restart ADB Server
            `'`'`'`---..,___`.  {0}[{1}9{0}] {2}Pull files from device       
                      
-{0}[{1}10{0}] {2}Shutdown the device      {0}[{1}19{0}]{2} Extract apk from app                         
-{0}[{1}11{0}] {2}Uninstall an app         {0}[{1}20{0}]{2} Get Battery Status
-{0}[{1}12{0}] {2}Show device log          {0}[{1}21{0}]{2} Network status
-{0}[{1}13{0}] {2}Dump System Info         {0}[{1}22{0}]{2} Turn WiFi on/off
-{0}[{1}14{0}] {2}List all device apps     {0}[{1}23{0}]{2} Remove Password
-{0}[{1}15{0}] {2}Run a device app         {0}[{1}24{0}]{2} Use Ghost Keycode
-{0}[{1}16{0}]{2} Port Forwarding          {0}[{1}25{0}]{2} Get Current Activity
-{0}[{1}17{0}]{2} Grab wpa_supplicant      {0}[{1}26{0}]{2} Update Ghost Framework
-{0}[{1}18{0}]{2} Show Mac/Inet            {0}[{1}27{0}]{2} Exit Ghost Framework
+ {0}[{1}10{0}] {2}Shutdown the device      {0}[{1}19{0}]{2} Extract apk from app                         
+ {0}[{1}11{0}] {2}Uninstall an app         {0}[{1}20{0}]{2} Get Battery Status
+ {0}[{1}12{0}] {2}Show device log          {0}[{1}21{0}]{2} Network status
+ {0}[{1}13{0}] {2}Dump System Info         {0}[{1}22{0}]{2} Turn WiFi on/off
+ {0}[{1}14{0}] {2}List all device apps     {0}[{1}23{0}]{2} Remove Password
+ {0}[{1}15{0}] {2}Run a device app         {0}[{1}24{0}]{2} Use Ghost Keycode
+ {0}[{1}16{0}]{2} Port Forwarding          {0}[{1}25{0}]{2} Get Current Activity
+ {0}[{1}17{0}]{2} Grab wpa_supplicant      {0}[{1}26{0}]{2} Update Ghost Framework
+ {0}[{1}18{0}]{2} Show Mac/Inet            {0}[{1}27{0}]{2} Exit Ghost Framework
 '''.format(GNSL, REDL, WHSL)
 
 page_2 = '''\n
@@ -411,60 +411,60 @@ def main():
                 print (("{1}[{0}+{1}]{2} No devices connected yet.").format(REDL, GNSL, WHSL))
                 main()
             print ('''
-    0   -->  KEYCODE_UNKNOWN
-    1   -->  KEYCODE_MENU
-    2   -->  KEYCODE_SOFT_RIGHT
-    3   -->  KEYCODE_HOME
-    4   -->  KEYCODE_BACK
-    5   -->  KEYCODE_CALL
-    6   -->  KEYCODE_ENDCALL
-    7   -->  KEYCODE_0
-    8   -->  KEYCODE_1
-    9   -->  KEYCODE_2
-    10  -->  KEYCODE_3
-    11  -->  KEYCODE_4
-    12  -->  KEYCODE_5
-    13  -->  KEYCODE_6
-    14  -->  KEYCODE_7
-    15  -->  KEYCODE_8
-    16  -->  KEYCODE_9
-    17  -->  KEYCODE_STAR
-    18  -->  KEYCODE_POUND
-    19  -->  KEYCODE_DPAD_UP
-    20  -->  KEYCODE_DPAD_DOWN
-    21  -->  KEYCODE_DPAD_LEFT
-    22  -->  KEYCODE_DPAD_RIGHT
-    23  -->  KEYCODE_DPAD_CENTER
-    24  -->  KEYCODE_VOLUME_UP
-    25  -->  KEYCODE_VOLUME_DOWN
-    26  -->  KEYCODE_POWER
-    27  -->  KEYCODE_CAMERA
-    28  -->  KEYCODE_CLEAR
-    29  -->  KEYCODE_A
-    30  -->  KEYCODE_B
-    31  -->  KEYCODE_C
-    32  -->  KEYCODE_D
-    33  -->  KEYCODE_E
-    34  -->  KEYCODE_F
-    35  -->  KEYCODE_G
-    36  -->  KEYCODE_H
-    37  -->  KEYCODE_I
-    38  -->  KEYCODE_J
-    39  -->  KEYCODE_K
-    40  -->  KEYCODE_L
-    41  -->  KEYCODE_M
-    42  -->  KEYCODE_N
-    43  -->  KEYCODE_O
-    44  -->  KEYCODE_P
-    45  -->  KEYCODE_Q
-    46  -->  KEYCODE_R
-    47  -->  KEYCODE_S
-    48  -->  KEYCODE_T
-    49  -->  KEYCODE_U
-    50  -->  KEYCODE_V
-    51  -->  KEYCODE_W
-    52  -->  KEYCODE_X
-    53  -->  KEYCODE_Y
+ 0   -->  KEYCODE_UNKNOWN
+ 1   -->  KEYCODE_MENU
+ 2   -->  KEYCODE_SOFT_RIGHT
+ 3   -->  KEYCODE_HOME
+ 4   -->  KEYCODE_BACK
+ 5   -->  KEYCODE_CALL
+ 6   -->  KEYCODE_ENDCALL
+ 7   -->  KEYCODE_0
+ 8   -->  KEYCODE_1
+ 9   -->  KEYCODE_2
+ 10  -->  KEYCODE_3
+ 11  -->  KEYCODE_4
+ 12  -->  KEYCODE_5
+ 13  -->  KEYCODE_6
+ 14  -->  KEYCODE_7
+ 15  -->  KEYCODE_8
+ 16  -->  KEYCODE_9
+ 17  -->  KEYCODE_STAR
+ 18  -->  KEYCODE_POUND
+ 19  -->  KEYCODE_DPAD_UP
+ 20  -->  KEYCODE_DPAD_DOWN
+ 21  -->  KEYCODE_DPAD_LEFT
+ 22  -->  KEYCODE_DPAD_RIGHT
+ 23  -->  KEYCODE_DPAD_CENTER
+ 24  -->  KEYCODE_VOLUME_UP
+ 25  -->  KEYCODE_VOLUME_DOWN
+ 26  -->  KEYCODE_POWER
+ 27  -->  KEYCODE_CAMERA
+ 28  -->  KEYCODE_CLEAR
+ 29  -->  KEYCODE_A
+ 30  -->  KEYCODE_B
+ 31  -->  KEYCODE_C
+ 32  -->  KEYCODE_D
+ 33  -->  KEYCODE_E
+ 34  -->  KEYCODE_F
+ 35  -->  KEYCODE_G
+ 36  -->  KEYCODE_H
+ 37  -->  KEYCODE_I
+ 38  -->  KEYCODE_J
+ 39  -->  KEYCODE_K
+ 40  -->  KEYCODE_L
+ 41  -->  KEYCODE_M
+ 42  -->  KEYCODE_N
+ 43  -->  KEYCODE_O
+ 44  -->  KEYCODE_P
+ 45  -->  KEYCODE_Q
+ 46  -->  KEYCODE_R
+ 47  -->  KEYCODE_S
+ 48  -->  KEYCODE_T
+ 49  -->  KEYCODE_U
+ 50  -->  KEYCODE_V
+ 51  -->  KEYCODE_W
+ 52  -->  KEYCODE_X
+ 53  -->  KEYCODE_Y
     54  -->  KEYCODE_Z
     55  -->  KEYCODE_COMMA
     56  -->  KEYCODE_PERIOD
