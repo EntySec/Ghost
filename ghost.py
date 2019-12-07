4e95dba70a09c7031e87b44da7b6c7262bb84c39
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-08 00:44:43 +0300

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -126,8 +126,6 @@ page_2 = '''\n
 #=============================
 #Main
 def main():
-    import os
-    os.system("printf '\033]2;Ghost Framework\a'")
     page_num = 1
     option = raw_input(WHSL + "ghost"+GNSL+"("+REDL + "main_menu" + GNSL + ")"+WHSL + "> ")
         
@@ -467,38 +465,38 @@ def main():
  51  -->  KEYCODE_W
  52  -->  KEYCODE_X
  53  -->  KEYCODE_Y
-    54  -->  KEYCODE_Z
-    55  -->  KEYCODE_COMMA
-    56  -->  KEYCODE_PERIOD
-    57  -->  KEYCODE_ALT_LEFT
-    58  -->  KEYCODE_ALT_RIGHT
-    59  -->  KEYCODE_SHIFT_LEFT
-    60  -->  KEYCODE_SHIFT_RIGHT
-    61  -->  KEYCODE_TAB
-    62  -->  KEYCODE_SPACE
-    63  -->  KEYCODE_SYM
-    64  -->  KEYCODE_EXPLORER
-    65  -->  KEYCODE_ENVELOPE
-    66  -->  KEYCODE_ENTER
-    67  -->  KEYCODE_DEL
-    68  -->  KEYCODE_GRAVE
-    69  -->  KEYCODE_MINUS
-    70  -->  KEYCODE_EQUALS
-    71  -->  KEYCODE_LEFT_BRACKET
-    72  -->  KEYCODE_RIGHT_BRACKET
-    73  -->  KEYCODE_BACKSLASH
-    74  -->  KEYCODE_SEMICOLON
-    75  -->  KEYCODE_APOSTROPHE
-    76  -->  KEYCODE_SLASH
-    77  -->  KEYCODE_AT
-    78  -->  KEYCODE_NUM
-    79  -->  KEYCODE_HEADSETHOOK
-    80  -->  KEYCODE_FOCUS
-    81  -->  KEYCODE_PLUS
-    82  -->  KEYCODE_MENU
-    83  -->  KEYCODE_NOTIFICATION
-    84  -->  KEYCODE_SEARCH
-    85  -->  TAG_LAST_KEYCODE
+ 54  -->  KEYCODE_Z
+ 55  -->  KEYCODE_COMMA
+ 56  -->  KEYCODE_PERIOD
+ 57  -->  KEYCODE_ALT_LEFT
+ 58  -->  KEYCODE_ALT_RIGHT
+ 59  -->  KEYCODE_SHIFT_LEFT
+ 60  -->  KEYCODE_SHIFT_RIGHT
+ 61  -->  KEYCODE_TAB
+ 62  -->  KEYCODE_SPACE
+ 63  -->  KEYCODE_SYM
+ 64  -->  KEYCODE_EXPLORER
+ 65  -->  KEYCODE_ENVELOPE
+ 66  -->  KEYCODE_ENTER
+ 67  -->  KEYCODE_DEL
+ 68  -->  KEYCODE_GRAVE
+ 69  -->  KEYCODE_MINUS
+ 70  -->  KEYCODE_EQUALS
+ 71  -->  KEYCODE_LEFT_BRACKET
+ 72  -->  KEYCODE_RIGHT_BRACKET
+ 73  -->  KEYCODE_BACKSLASH
+ 74  -->  KEYCODE_SEMICOLON
+ 75  -->  KEYCODE_APOSTROPHE
+ 76  -->  KEYCODE_SLASH
+ 77  -->  KEYCODE_AT
+ 78  -->  KEYCODE_NUM
+ 79  -->  KEYCODE_HEADSETHOOK
+ 80  -->  KEYCODE_FOCUS
+ 81  -->  KEYCODE_PLUS
+ 82  -->  KEYCODE_MENU
+ 83  -->  KEYCODE_NOTIFICATION
+ 84  -->  KEYCODE_SEARCH
+ 85  -->  TAG_LAST_KEYCODE
             ''')
             print (("{1}[{0}+{1}]{2} Enter an option number.").format(REDL, GNSL, WHSL))
             num = raw_input(arrow + " ghost"+GNSL+"("+REDL + "keycode" + GNSL + ")"+WHSL + "> ")
@@ -536,6 +534,9 @@ def main():
 #=============================
 #=============================  
 # Run
+
+import os
+os.system("printf '\033]2;Ghost Framework\a'")
 print (("{1}[{0}+{1}]{2} Starting ADB server...").format(REDL, GNSL, WHSL))
 os.system("{ adb tcpip 5555; } &> /dev/null")
 t.sleep(4)
