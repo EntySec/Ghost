0b219e65801a11e14623781445e3194835ae02ee
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-06 00:51:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#            --------------------------------------------------
-#                             Ghost Framework                                                  
-#            --------------------------------------------------
-#               Copyright (C) <2019-2020>  <Entynetproject>
+#            ---------------------------------------------------
+#                              Ghost Framework                                                  
+#            ---------------------------------------------------
+#                Copyright (C) <2019-2020>  <Entynetproject>
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
