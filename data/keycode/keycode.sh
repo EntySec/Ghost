ab2c53386a66a5f800379997a1cce10628187e43
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-07 15:47:14 +0300

    Update keycode.sh

--- data/keycode/keycode.sh
+++ data/keycode/keycode.sh
@@ -19,7 +19,10 @@
 #        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 if [[ "$1" = "" ]]; then
-    echo "Usage: keycode.sh <keycode_number>"
+    echo "Usage: keycode.sh {hidden}"
+    echo
+    echo "Note: This file only for Ghost Framework, if you execute it without"
+    echo "Ghost Framework it will not work, this is only a part of main code."
 else
     {
     adb shell input keyevent $1
