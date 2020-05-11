7a5e95eed8fedc5951cfdb35f9d46d0920c594f4
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-11 12:21:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ then
 fi
 
 {
-ASESR="$( ping -c 1 -q google.com >&/dev/null; echo $? )"
+ASESR="$( ping -c 1 -q www.google.com >&/dev/null; echo $? )"
 } &> /dev/null
 if [[ "$ASESR" != 0 ]]
 then 
