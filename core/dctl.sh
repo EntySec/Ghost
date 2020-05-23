0ae5f9e41f956a14e64814f1720ad27667a85965
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-23 15:46:49 +0300

    Create dctl.sh

---
+++ core/dctl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#            ---------------------------------------------------
+#                              Ghost Framework                                                  
+#            ---------------------------------------------------
+#                Copyright (C) <2019-2020>  <Entynetproject>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [[ "$1" = "" ]] 
+then
+    echo -e "Usage: dctl.sh [connect|disconnect] <target>"
+else
+    if [[ "$1" = "connect" ]]
+    then
+        if [[ "$2" = "" ]]
+        then
+            echo -e "Usage: dctl.sh [connect|disconnect] <target>"
+        else
+            {
+            adb connect $2
+            } &> /dev/null
+        fi
+    elif [[ "$1" = "disconnect" ]]
+    then
+        if [[ "$2" = "" ]]
+        then
+            echo -e "Usage: dctl.sh [connect|disconnect] <target>"
+        else
+            {
+            adb disconnect $2
+            } &> /dev/null
+        fi
+    else
+        echo -e "Usage: dctl.sh [connect|disconnect] <target>"
+    fi
+fi
