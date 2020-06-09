aaf7bb3b35d7d7039ce3a65fd4d9f0004dddbace
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-09 19:51:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,6 +59,7 @@ apt-get -y install git
 apt-get -y install python3
 apt-get -y install adb
 apt-get -y install android-tools-adb
+apt-get -y install android-tools-fastboot
 apk update
 apk add git
 apk add python3
