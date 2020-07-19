7cdd1720956d6f74671835199201e04f619954c6
Ivan Nikolsky <enty8080@gmail.com>
2020-07-19 18:48:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,6 +58,7 @@ pkg -y install android-tools
 apt-get update
 apt-get -y install git
 apt-get -y install python3
+apt-get -y install python3-pip
 apt-get -y install adb
 apt-get -y install android-tools-adb
 apt-get -y install android-tools-fastboot
@@ -65,28 +66,35 @@ apk update
 apk add git
 apk add python3
 apk add android-tools
+apk add py3-pip
 pacman -Sy
 pacman -S --noconfirm git
 pacman -S --noconfirm python3
 pacman -S --noconfirm android-tools
+pacman -S --noconfirm python3-pip
 zypper refresh
 zypper install -y git
 zypper install -y python3
 zypper install -y android-tools
+zypper install -y python3-pip
 yum -y install git
 yum -y install python3
 yum -y install android-tools
+yum -y install python3-pip
 dnf -y install git
 dnf -y install python3
 dnf -y install android-tools
+dnf -y install python3-pip
 eopkg update-repo
 eopkg -y install git
 eopkg -y install python3
 eopkg -y install android-tools
+eopkg -y install python3-pip
 xbps-install -S
 xbps-install -y git
 xbps-install -y python3
 xbps-install -y android-tools
+xbps-install -y python3-pip
 } &> /dev/null
 
 if [[ -d ~/ghost ]]
@@ -106,6 +114,11 @@ else
 echo -e ""$E"Installation failed!"
 exit
 fi
+
+{
+python3 -m pip install setuptools
+python3 -m pip install -r requirements.txt
+} &> /dev/null
 
 {
 cd bin
