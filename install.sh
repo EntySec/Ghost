be0a901903da243b0b2530a76e5d0fef88a3597e
Ivan Nikolsky <enty8080@gmail.com>
2020-07-20 21:19:04 +0300

    Revert "Merge pull request #146 from game-lover/master"
    This reverts commit f8bd0e8010a210a0877839c97313350b5eb47654, reversing
    changes made to 6f762a615e18033d215971ebd22a5ed1d9c49ad9.

--- install.sh
+++ install.sh
@@ -58,7 +58,6 @@ pkg -y install android-tools
 apt-get update
 apt-get -y install git
 apt-get -y install python3
-apt-get -y install python3-pip
 apt-get -y install adb
 apt-get -y install android-tools-adb
 apt-get -y install android-tools-fastboot
@@ -66,35 +65,28 @@ apk update
 apk add git
 apk add python3
 apk add android-tools
-apk add py3-pip
 pacman -Sy
 pacman -S --noconfirm git
 pacman -S --noconfirm python3
 pacman -S --noconfirm android-tools
-pacman -S --noconfirm python3-pip
 zypper refresh
 zypper install -y git
 zypper install -y python3
 zypper install -y android-tools
-zypper install -y python3-pip
 yum -y install git
 yum -y install python3
 yum -y install android-tools
-yum -y install python3-pip
 dnf -y install git
 dnf -y install python3
 dnf -y install android-tools
-dnf -y install python3-pip
 eopkg update-repo
 eopkg -y install git
 eopkg -y install python3
 eopkg -y install android-tools
-eopkg -y install python3-pip
 xbps-install -S
 xbps-install -y git
 xbps-install -y python3
 xbps-install -y android-tools
-xbps-install -y python3-pip
 } &> /dev/null
 
 if [[ -d ~/ghost ]]
@@ -114,15 +106,6 @@ else
 echo -e ""$E"Installation failed!"
 exit
 fi
-
-{
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-pip3 uninstall readline
-pip3 install gnureadline
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-pip3 install readline
-fi
-} &> /dev/null
 
 {
 cd bin
