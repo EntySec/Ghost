930e2d1a8bd24619aa57cef1bfd7f3c4b0ccf6ac
Jaxparrow <36300128+jaxparrow07@users.noreply.github.com>
2020-09-17 15:41:19 +0530

    Added scrcpy installation

--- install.sh
+++ install.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+#            ---------------------------------------------------
+#                              Ghost Framework                                                  
+#            ---------------------------------------------------
+#                Copyright (C) <2019-2020>  <Entynetproject>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 printf '\033]2;install.sh\a'
 
 G="\033[1;34m[*] \033[0m"
 S="\033[1;32m[+] \033[0m"
-I="\033[1;77m[i] \033[0m"
 E="\033[1;31m[-] \033[0m"
 
-if [[ $(id -u) != 0 ]]
+if [[ $EUID -ne 0 ]]
 then
-   echo -e ""$E"Permission denied!"
+   echo -e ""$E"This script must be run as root!"
    exit
 fi
 
@@ -41,8 +58,7 @@ apt-get update
 apt-get -y install git
 apt-get -y install python3
 apt-get -y install adb
-apt-get -y install android-tools-adb
-apt-get -y install android-tools-fastboot
+apt-get -y install scrcpy
 apk update
 apk add git
 apk add python3
