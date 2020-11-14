f6039806af4ce4977f96c6d568b98943249cf79f
Ivan Nikolsky <enty8080@gmail.com>
2020-11-14 13:13:09 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,16 +7,15 @@ S="\033[1;32m[+] \033[0m"
 I="\033[1;77m[i] \033[0m"
 E="\033[1;31m[-] \033[0m"
 
-if [[ $(id -u) != 0 ]]
-then
-   echo -e ""$E"Permission denied!"
-   exit
+if [[ $(id -u) != 0 ]]; then
+    echo -e ""$E"Permission denied!"
+    exit
 fi
 
 {
-rm /bin/ghost
-rm /usr/local/bin/ghost
-rm -rf ~/ghost
-rm /etc/ghost.conf
-rm /data/data/com.termux/files/usr/bin/ghost
+    rm /usr/bin/ghost
+   rm /usr/local/bin/ghost
+   rm -rf ~/ghost
+   rm /etc/ghost.conf
+   rm /data/data/com.termux/files/usr/bin/ghost
 } &> /dev/null
