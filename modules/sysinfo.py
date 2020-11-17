2b41ea02faed2ee664d3d0497876dd6ae98439e9
Ivan Nikolsky <enty8080@gmail.com>
2020-11-17 20:48:32 +0300

    Update sysinfo.py

--- modules/sysinfo.py
+++ modules/sysinfo.py
@@ -39,15 +39,15 @@ class GhostModule:
         self.args = 1
 
     def run(self, cmd_data):
-        system = self.ghost.send_command("shell", "uname -s")
-        hostname = self.ghost.send_command("shell", "uname -n")
-        username = self.ghost.send_command("shell", "whoami")
-        version = self.ghost.send_command("shell", "uname -r")
-        architecture = self.ghost.send_command("shell", "uname -m")
+        system = "Android"
+        hostname = self.ghost.send_command("shell", "getprop net.hostname")
+        username = self.ghost.send_command("shell", "getprop ro.product.name")
+        version = self.ghost.send_command("shell", "getprop ro.build.version.release")
+        architecture = self.ghost.send_command("shell", "getprop ro.product.cpu.abi")
         information = ""
         information += f"{self.badges.I}Operating System: {system}\n"
         information += f"{self.badges.I}Computer Hostname: {hostname}\n"
         information += f"{self.badges.I}Computer Username: {username}\n"
         information += f"{self.badges.I}Release Version: {version}\n"
-        information += f"{self.badges.I}Processor Architecture: {architecture}\n"
+        information += f"{self.badges.I}Processor Architecture: {architecture}"
         print(information)
