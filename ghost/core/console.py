39e02756fbe8cd47e101a449aea2349ac5e35810
Ivan Nikolsky <enty8080@gmail.com>
2021-08-03 17:40:17 +0200

    Update console.py

--- ghost/core/console.py
+++ ghost/core/console.py
@@ -28,7 +28,6 @@ import os
 import readline
 
 from ghost.core.badges import Badges
-from ghost.core.server import Server
 from ghost.core.helper import Helper
 from ghost.core.ghost import Ghost
 
@@ -36,9 +35,7 @@ from ghost.core.ghost import Ghost
 class Console:
     def __init__(self):
         self.badges = Badges()
-        self.server = Server()
         self.helper = Helper()
-        self.ghost = Ghost()
 
     def banner(self):
         print("""
@@ -53,67 +50,19 @@ class Console:
         print("Ghost Framework " + self.helper.version)
         print("--------------------")
 
-        print("""
-[*] Also explore our last projects:
-    * HatSploit (https://github.com/EntySec/HatSploit) - Modular exploitation
-    * HatVenom  (https://github.com/EntySec/HatVenom)  - Binary exploitation
-    * Shreder   (https://github.com/EntySec/Shreder)   - SSH bruteforce
-    * RomBuster (https://github.com/EntySec/RomBuster) - Router hacking
-    * CamOver   (https://github.com/EntySec/CamOver)   - Camera hacking
-    * CamRaptor (https://github.com/EntySec/CamRaptor) - Camera hacking
-[i] Please rate my work by star :)
-
-    Our Twitter: @entysec
-    CEO Twitter: @enty8080
-""")
-
     def shell(self):
          readline.parse_and_bind('tab: complete')
          while True:
-             try:
-                 ui = input('\033[4mghost\033[0m> ').strip(" ")
-                 ui = ui.split()
+            try:
+                command = input('ghost> ').strip()
+                command = command.split()
 
-                 if not ui:
-                     continue
-                 elif ui[0] == "exit":
-                     break
-                 elif ui[0] == "clear":
-                     os.system("clear")
-                 elif ui[0] == "help":
-                     print("")
-                     print("Core Commands")
-                     print("=============")
-                     print("")
-                     print("    Command        Description")
-                     print("    -------        -----------")
-                     print("    clear          Clear terminal window.")
-                     print("    connect        Connect to the specified device.")
-                     print("    disconnect     Disconnect specified device.")
-                     print("    exit           Exit Ghost Framework.")
-                     print("    help           Show available commands.")
-                     print("")
-                 elif ui[0] == "connect":
-                     if len(ui) < 2:
-                         print("Usage: connect <address>")
-                     else:
-                         try:
-                             if len(ui[1].split(':')) < 2:
-                                 self.server.connect(ui[1], 5555)
-                             else:
-                                 self.server.connect(ui[1].split(':')[0],  ui[1].split(':')[1])
-                         except SystemExit:
-                             pass
-                 elif ui[0] == "disconnect":
-                     if len(ui) < 2:
-                         print("Usage: disconnect <address>")
-                     else:
-                         try:
-                             self.ghost.disconnect(ui[1].split(':')[0])
-                         except SystemExit:
-                             pass
-                 else:
-                     print(self.badges.E + "Unrecognized command!")
+                if command[0] == 'connect':
+                    if len(command) < 2:
+                        self.badges.output_empty("Usage: connect <>")
+
+                else:
+                    print(self.badges.E + "Unrecognized command!")
              except (EOFError, KeyboardInterrupt):
                  pass
              except Exception as e:
