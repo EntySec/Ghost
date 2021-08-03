74703b8448c5079c0c5dcd6008b8627f67bb794e
Ivan Nikolsky <enty8080@gmail.com>
2021-08-03 23:16:59 +0200

    Update battery.py

--- ghost/modules/battery.py
+++ ghost/modules/battery.py
@@ -1,50 +1,35 @@
 #!/usr/bin/env python3
 
 #
-# MIT License
-#
-# Copyright (c) 2020 EntySec
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
 #
 
-from ghost.core.badges import Badges
+from ghost.core.cli.badges import Badges
 
 
 class GhostModule:
-    def __init__(self, ghost):
-        self.ghost = ghost
+    def __init__(self, device):
+        self.device = device
         self.badges = Badges()
 
         self.details = {
-            'name': "battery",
-            'authors': ['enty8080'],
-            'description': "Show device battery state.",
-            'usage': "battery",
-            'type': "settings",
-            'args': 0,
-            'needs_args': False,
-            'needs_root': False,
-            'comments': ""
+            'Category': "settings",
+            'Name': "battery",
+            'Authors': [
+                'enty8080'
+            ],
+            'Description': "Show device battery state.",
+            'Comments': [
+                ''
+            ],
+            'Usage': "battery",
+            'MinArgs': 0,
+            'NeedsRoot': False
         }
 
     def run(self):
-        output = self.ghost.send_command("shell", "dumpsys battery")
-        print(self.badges.I + "Device Battery Information:")
-        print(output)
+        output = self.ghost.send_command("dumpsys battery")
+
+        self.badges.print_information("Device Battery Information:")
+        self.badges.print_empty(output)
