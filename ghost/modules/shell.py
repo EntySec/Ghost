31da05b627b0a397b5621f1b2dcb551678ca8a47
Ivan Nikolsky <enty8080@gmail.com>
2021-08-04 18:27:54 +0200

    Create shell.py

---
+++ ghost/modules/shell.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
+#
+
+from ghost.lib.module import Module
+
+
+class GhostModule(Module):
+    details = {
+        'Category': "manage",
+        'Name': "shell",
+        'Authors': [
+            'enty8080'
+        ],
+        'Description': "Execute shell command on device.",
+        'Comments': [
+            ''
+        ],
+        'Usage': "shell <command>",
+        'MinArgs': 1,
+        'NeedsRoot': False
+    }
+
+    def run(self, argc, argv):
+        output = self.device.send_command(argv[0])
+        self.print_empty(output)
