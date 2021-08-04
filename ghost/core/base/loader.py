b1c4f7d839ec1970e6aae287d4eb79532bb9b02a
Ivan Nikolsky <enty8080@gmail.com>
2021-08-04 18:12:24 +0200

    Update loader.py

--- ghost/core/base/loader.py
+++ ghost/core/base/loader.py
@@ -41,8 +41,9 @@ class Loader:
                     spec = importlib.util.spec_from_file_location(path + '/' + mod, path + '/' + mod)
                     module = importlib.util.module_from_spec(spec)
                     spec.loader.exec_module(module)
-                    module = module.GhostModule(device)
+                    module = module.GhostModule()
 
+                    module.device = device
                     modules[module.details['Name']] = module
                 except Exception as e:
                     print(e)
