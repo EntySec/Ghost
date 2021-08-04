23fdcf57ecf75a1715d2a76a9943eacf29210bf4
Ivan Nikolsky <enty8080@gmail.com>
2021-08-04 18:21:18 +0200

    Create battery.py

---
+++ ghost/modules/battery.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
+#
+
+from ghost.lib.module import Module
+
+
+class GhostModule(Module):
+    details = {
+        'Category': "settings",
+        'Name': "battery",
+        'Authors': [
+            'enty8080'
+        ],
+        'Description': "Show device battery information.",
+        'Comments': [
+            ''
+        ],
+        'Usage': "battery",
+        'MinArgs': 0,
+        'NeedsRoot': False
+    }
+
+    def run(self, argc, argv):
+        self.print_process("Getting battery information...")
+
+        output = self.device.send_command("dumpsys battery")
+        self.print_empty(output)
