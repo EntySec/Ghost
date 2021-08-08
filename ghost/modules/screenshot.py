625156105bfcbb3629b30ec029045ec61fb5e479
Ivan Nikolsky <enty8080@gmail.com>
2021-08-08 15:35:21 +0200

    Update screenshot.py

--- ghost/modules/screenshot.py
+++ ghost/modules/screenshot.py
@@ -31,15 +31,15 @@ class GhostModule(Module):
 
     def run(self, argc, argv):
         self.print_process(f"Taking screenshot...")
-        self.device.send_command("screencap /tmp/screenshot.png")
+        self.device.send_command("screencap /data/local/tmp/screenshot.png")
 
         exists, file = self.fs.exists_directory(argv[0])
         if exists and file == 'file':
-            if self.device.download('/tmp/screenshot.png', argv[0]):
+            if self.device.download('/data/local/tmp/screenshot.png', argv[0]):
                 self.print_success("Screenshot has been downloaded!")
 
         if exists and file == 'directory':
-            if self.device.download('/tmp/screenshot.png', argv[0] + '/screenshot.png'):
+            if self.device.download('/data/local/tmp/screenshot.png', argv[0] + '/screenshot.png'):
                 self.print_success("Screenshot has been downloaded!")
                 
-        self.device.send_command("rm /tmp/screenshot.png")
+        self.device.send_command("rm /data/local/tmp/screenshot.png")
