f6249bbfc345495b3792c2344e51f3e73dbb418f
Ivan Nikolsky <enty8080@gmail.com>
2021-08-08 15:53:18 +0200

    Create press.py

---
+++ ghost/modules/press.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
+#
+
+from ghost.lib.module import Module
+
+
+class GhostModule(Module):
+    details = {
+        'Category': "manage",
+        'Name': "press",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Press device button by keycode.",
+        'Comments': [
+            ''
+        ],
+        'Usage': "press <keycode>",
+        'MinArgs': 1,
+        'NeedsRoot': False
+    }
+
+    def run(self, argc, argv):
+        if int(argv[0]) < 124:
+            self.device.send_command(f"input keyevent {argv[0]}")
+        else:
+            self.print_error("Invalid keycode!")
