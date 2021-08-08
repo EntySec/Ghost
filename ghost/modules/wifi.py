fe3cb8e2a253b2790e31db8af61e084143e2a24e
Ivan Nikolsky <enty8080@gmail.com>
2021-08-08 14:57:27 +0200

    Create wifi.py

---
+++ ghost/modules/wifi.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
+#
+
+from ghost.lib.module import Module
+
+
+class GhostModule(Module):
+    details = {
+        'Category': "settings",
+        'Name': "wifi",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Set device wifi service state.",
+        'Comments': [
+            ''
+        ],
+        'Usage': "wifi <on|off>",
+        'MinArgs': 1,
+        'NeedsRoot': False
+    }
+
+    def run(self, argc, argv):
+        if argv[0] in ['on', 'off']:
+            if argv[0] == 'on':
+                self.device.send_command("svc wifi enable")
+            else:
+                self.device.send_command("svc wifi disable")
+        else:
+            self.print_empty(f"Usage: {self.details['Usage']}")
