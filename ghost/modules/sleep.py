4ffe29412c8b4964ada77bd170cf9e1123208bb0
Ivan Nikolsky <enty8080@gmail.com>
2021-08-08 15:55:46 +0200

    Create sleep.py

---
+++ ghost/modules/sleep.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
+#
+
+from ghost.lib.module import Module
+
+
+class GhostModule(Module):
+    details = {
+        'Category': "manage",
+        'Name': "sleep",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Put device into sleep mode.",
+        'Comments': [
+            ''
+        ],
+        'Usage': "sleep",
+        'MinArgs': 0,
+        'NeedsRoot': False
+    }
+
+    def run(self, argc, argv):
+        self.device.send_command("input keyevent 26")
