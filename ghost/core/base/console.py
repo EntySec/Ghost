d7223c788305b4c3bbfdd6e73959a123d179ad1f
Ivan Nikolsky <enty8080@gmail.com>
2021-08-08 17:17:06 +0200

    Update console.py

--- ghost/core/base/console.py
+++ ghost/core/base/console.py
@@ -104,6 +104,8 @@ class Console:
                                     'device': device
                                 }
                             })
+                            self.badges.print_empty("")
+
                             self.badges.print_information(f"Type {self.colors.GREEN}devices{self.colors.END} to list all connected devices.")
                             self.badges.print_information(f"Type {self.colors.GREEN}interact {str(len(self.devices)-1) + self.colors.END} to interact this device.")
 
