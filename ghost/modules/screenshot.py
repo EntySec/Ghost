42c117cd25086a0cffcbd81cffef551abb0bec08
Ivan Nikolsky <enty8080@gmail.com>
2021-08-08 15:11:42 +0200

    Create screenshot.py

---
+++ ghost/modules/screenshot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#
+# This module requires Ghost: https://github.com/EntySec/Ghost
+# Current source: https://github.com/EntySec/Ghost
+#
+
+import os
+
+from ghost.lib.module import Module
+from ghost.utils.fs import FSTools
+
+
+class GhostModule(Module):
+    fs = FSTools()
+
+    details = {
+        'Category': "manage",
+        'Name': "screenshot",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Take device screenshot.",
+        'Comments': [
+            ''
+        ],
+        'Usage': "screenshot <local_path>",
+        'MinArgs': 1,
+        'NeedsRoot': False
+    }
+
+    def run(self, argc, argv):
+        self.print_process(f"Taking screenshot...")
+        self.device.send_command("screencap /tmp/screenshot.png")
+
+        exists, file = self.fs.exists_directory(argv[0])
+        if exists and file == 'file':
+            if self.device.download('/tmp/screenshot.png', argv[0]):
+                self.print_success("Screenshot has been downloaded!")
+
+        if exists and file == 'directory':
+            if self.device.download('/tmp/screenshot.png', argv[0] + '/screenshot.png'):
+                self.print_success("Screenshot has been downloaded!")
+                
+        self.device.send_command("rm /tmp/screenshot.png")
