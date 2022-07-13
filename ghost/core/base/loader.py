57c0f41892d5dcb4c0c9634849b4497d4fd82f91
Loubaris <adamou.loubaris@gmail.com>
2022-07-13 22:43:01 +0200

    Update loader.py

--- ghost/core/base/loader.py
+++ ghost/core/base/loader.py
@@ -58,7 +58,7 @@ class Loader:
 
                     module.device = device
                     modules[module.details['Name']] = module
-                except Exception:
+                except:
                     pass
         return modules
 
