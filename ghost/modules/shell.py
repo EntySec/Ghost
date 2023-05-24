9e84d000bf219e047e35be7da94dc97fd256b003
Ivan Nikolsky <enty8080@gmail.com>
2023-05-24 17:56:04 +0300

    Update shell.py

--- ghost/modules/shell.py
+++ ghost/modules/shell.py
@@ -10,7 +10,7 @@ class GhostModule(Module):
     def __init__(self):
         super().__init__()
 
-        self.details = {
+        self.details.update({
             'Category': "manage",
             'Name': "shell",
             'Authors': [
@@ -20,7 +20,7 @@ class GhostModule(Module):
             'Usage': "shell <command>",
             'MinArgs': 1,
             'NeedsRoot': False
-        }
+        })
 
     def run(self, argc, argv):
         output = self.device.send_command(' '.join(argv[1:]))
