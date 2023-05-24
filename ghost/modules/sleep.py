744d42c2e17abf53ccd5d16cf97aef18be679ee1
Ivan Nikolsky <enty8080@gmail.com>
2023-05-24 17:56:18 +0300

    Update sleep.py

--- ghost/modules/sleep.py
+++ ghost/modules/sleep.py
@@ -10,7 +10,7 @@ class GhostModule(Module):
     def __init__(self):
         super().__init__()
 
-        self.details = {
+        self.details.update({
             'Category': "manage",
             'Name': "sleep",
             'Authors': [
@@ -20,7 +20,7 @@ class GhostModule(Module):
             'Usage': "sleep",
             'MinArgs': 0,
             'NeedsRoot': False
-        }
+        })
 
     def run(self, argc, argv):
         self.device.send_command("input keyevent 26")
