5146b0efb44e9e0237e491bf6938aaf8844ec3e3
Ivan Nikolsky <enty8080@gmail.com>
2023-05-24 17:56:35 +0300

    Update upload.py

--- ghost/modules/upload.py
+++ ghost/modules/upload.py
@@ -12,7 +12,7 @@ class GhostModule(Module):
     def __init__(self):
         super().__init__()
 
-        self.details = {
+        self.details.update({
             'Category': "manage",
             'Name': "upload",
             'Authors': [
@@ -22,7 +22,7 @@ class GhostModule(Module):
             'Usage': "upload <local_file> <remote_path>",
             'MinArgs': 2,
             'NeedsRoot': False
-        }
+        })
 
     def run(self, argc, argv):
         self.device.upload(argv[1], argv[2])
