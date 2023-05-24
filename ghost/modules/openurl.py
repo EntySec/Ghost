4c0eb089a82be164bc8b0d6aacf3ea3b97f3d0a1
Ivan Nikolsky <enty8080@gmail.com>
2023-05-24 17:55:18 +0300

    Update openurl.py

--- ghost/modules/openurl.py
+++ ghost/modules/openurl.py
@@ -10,7 +10,7 @@ class GhostModule(Module):
     def __init__(self):
         super().__init__()
 
-        self.details = {
+        self.details.update({
             'Category': "manage",
             'Name': "openurl",
             'Authors': [
@@ -20,7 +20,7 @@ class GhostModule(Module):
             'Usage': "openurl <url>",
             'MinArgs': 1,
             'NeedsRoot': False
-        }
+        })
 
     def run(self, argc, argv):
         if not argv[1].startswith(("http://", "https://")):
