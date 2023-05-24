1537f2ef78fd1278d02145df4986ff7c908bf0c6
Ivan Nikolsky <enty8080@gmail.com>
2023-05-24 17:55:50 +0300

    Update screenshot.py

--- ghost/modules/screenshot.py
+++ ghost/modules/screenshot.py
@@ -12,7 +12,7 @@ class GhostModule(Module):
     def __init__(self):
         super().__init__()
 
-        self.details = {
+        self.details.update({
             'Category': "manage",
             'Name': "screenshot",
             'Authors': [
@@ -22,7 +22,7 @@ class GhostModule(Module):
             'Usage': "screenshot <local_path>",
             'MinArgs': 1,
             'NeedsRoot': False
-        }
+        })
 
     def run(self, argc, argv):
         self.print_process(f"Taking screenshot...")
